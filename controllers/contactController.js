137c8d362edda9324e36d483ef94417c7ab1e1af
Nadim Chowdhury <nadim-chowdhury@outlook.com>
2023-05-27 20:54:18 +0600

    major bug fixed

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -26,6 +26,7 @@ const createContact = asyncHandler(async (req, res) => {
     name,
     email,
     phone,
+    user_id: req.user.id,
   });
   res.json(contact);
 });
@@ -36,6 +37,12 @@ const updateContact = asyncHandler(async (req, res) => {
     res.status(404);
     throw new Error("Contact not found");
   }
+
+  if (contact.user_id.toString() !== req.user.id) {
+    res.status(403);
+    throw new Error("User don't have permission to updated");
+  }
+
   const updatedContact = await Contact.findByIdAndUpdate(
     req.params.id,
     req.body,
@@ -50,7 +57,13 @@ const deleteContact = asyncHandler(async (req, res) => {
     res.status(404);
     throw new Error("Contact not found");
   }
-  await contact.remove();
+
+  if (contact.user_id.toString() !== req.user.id) {
+    res.status(403);
+    throw new Error("User don't have permission to delete");
+  }
+
+  await contact.deleteOne({ _id: req.params.id });
   res.json(contact);
 });
 
